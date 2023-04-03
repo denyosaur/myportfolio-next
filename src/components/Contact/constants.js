f5552daccd77272727f98689c24be2c6dc9d69c3
denyosaur <danielkim1989@gmail.com>
2023-04-03 15:40:21 -0700

    contact icon constants

--- src/components/Contact/constants.js
+++ src/components/Contact/constants.js
@@ -1,13 +1,13 @@
-import Email from '/public/icons/Email.svg'
-import Facebook from '/public/icons/Facebook.svg'
-import Github from '/public/icons/Github.svg'
-import Instagram from '/public/icons/Instagram.svg'
-import Linkedin from '/public/icons/Linkedin.svg'
+import Email from '/public/icons/email.svg';
+import Facebook from '/public/icons/facebook.svg';
+import Github from '/public/icons/github.svg';
+import Instagram from '/public/icons/instagram.svg';
+import Linkedin from '/public/icons/linkedin.svg';
 
 export const iconDictionary = {
-  email: Email,
-  facebook: Facebook,
-  github: Github,
-  instagram: Instagram,
-  linkedin: Linkedin,
+  email: email,
+  facebook: facebook,
+  github: github,
+  instagram: instagram,
+  linkedin: linkedin,
 };
