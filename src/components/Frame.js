c572dfeef92bd37928bd0642c1e6ef7043221b5c
denyosaur <operationdaniel@gmail.com>
2023-04-08 16:12:21 -0700

    external iconrotate

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,6 +1,6 @@
 function Frame() {
   return (
-    <div className="absolute border border-[#F1DAC4] w-[95vw] h-[83vh] s:h-[94vh] m-auto inset-x-0 inset-y-0 z-[2]">
+    <div className="absolute border border-[#F1DAC4] w-[95vw] h-[83vh] sm:h-[94vh] m-auto inset-x-0 inset-y-0 z-[2]">
     </div>
   )
 }
